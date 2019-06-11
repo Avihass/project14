fc5c7cfe8c28c9f98b9b7ec0cb67f408515f72d7
Avidan <avihass@outlook.com>
2019-06-11 23:13:33 +0300

    Initial Commit

---
+++ mmn14/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mmn14
+//
+//  Created by Avidan Hassine on 11/06/2019.
+//  Copyright © 2019 Avidan Hassine. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
