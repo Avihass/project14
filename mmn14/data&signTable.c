0f25960f3b5493e6c445d54f442aef42a028f8d9
Avidan <avihass@outlook.com>
2019-07-04 05:08:29 +0300

    -add signTable constructor -add addSign function

--- mmn14/data&signTable.c
+++ mmn14/data&signTable.c
@@ -1,19 +1,60 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "readSource.h"
 #include "data&signTable.h"
+#include "utils.h"
 
-signTab signTabConst() {
+void signTabCtor(signTabPtr* sign) {
     
+    signTabPtr signTmp;
+    signTmp = (signTabPtr) malloc(sizeof(signTab));
     
+    if (!signTmp)
+        printError("can't allocate memory");
+    
+    /* initialize the sign name */
+    strcpy(signTmp->sign, "");
+    
+    *sign = &(*signTmp);
 }
 
-void addSign(signTab* tabPtr, char* signStr, int type, int val) {
+void addSign(signTabPtr* head, char* signStr, int type, int val) {
     
+    /* if the sign table is empty */
+    if (strcmp((*head)->sign, "")) {
+        
+        strcpy((*head)->sign, signStr);
+        (*head)->dataType = type;
+        (*head)->value = val;
+    }
     
+    else {
+        
+        /* create new sign */
+        signTabPtr signTmp;
+        signTabPtr signIndex = (*head)->next;
+        signTmp = (signTabPtr) malloc(sizeof(signTab));
+        
+        if (!signTmp)
+            printError("can't allocate memory");
+        
+        strcpy(signTmp->sign, signStr);
+        signTmp->dataType = type;
+        signTmp->value = val;
+        
+        /* go to end of the table */
+        while (signIndex != NULL) {
+            signIndex = signIndex->next;
+        }
+        
+        /* add the sign */
+        signIndex->next = &(*signTmp);
+    }
 }
 
 int isAvailable(signTab st, char* signStr) {
     
+    return 0;
 }
--- mmn14/data&signTable.h
+++ mmn14/data&signTable.h
@@ -8,6 +8,11 @@ typedef struct signTable {
     char sign[MAX_LINE];
     int dataType;
     int value;
+    signTabPtr next;
 } signTab;
+
+
+void signTabCtor(signTabPtr* sign); /* construct a first node in a signTabPtr */
+void addSign(signTabPtr* head, char* signStr, int type, int val);
 
 #endif /* data_signTable_h */
