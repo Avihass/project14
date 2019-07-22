7e899cbfcc4dc9327f015fdbfb015e5e1cdce858
harel <harelbaruchi@gmail.com>
2019-07-22 16:44:51 +0300

    branch duplicated
    avidan's branch was duplicated for harel to work on

---
+++ assembly.c
@@ -0,0 +1,32 @@
+##include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "assembly.h"
+#include "readSource.h"
+#include "utils.h"
+
+int main(int argc, const char * argv[]) {
+
+    char srcFileName[MAX_FILE_NAME + 3];
+    FILE *srcFile;
+    int readedFirstWord;
+    int endOfSrc = 0;
+
+    if (strlen(argv[1]) >= MAX_FILE_NAME)
+        printError("the source file name is to long");
+
+    strcpy(srcFileName, argv[1]);
+    strcat(srcFileName, ".as");
+
+    if (!(srcFile = fopen("src.as", "r")))
+        printError("can't open the source file");
+
+    while (!endOfSrc) {
+
+        readedFirstWord = readFirstWord(&srcFile);
+
+        endOfSrc = 1;
+    }
+
+    return 0;
+}
---
+++ data&signTable.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "readSource.h"
+#include "data&signTable.h"
+#include "utils.h"
+
+/* constructor for signTab pointer, allocate a node with empty values */
+void signTabCtor(signTabPtr* sign) {
+
+    signTabPtr signTmp;
+    signTmp = (signTabPtr) malloc(sizeof(signTab));
+
+    if (!signTmp)
+        printError("can't allocate memory");
+
+    /* initialize the sign name */
+    strcpy(signTmp->sign, "");
+
+    *sign = &(*signTmp);
+}
+
+void addSign(signTabPtr head, char* signStr, int type, int val) {
+
+    /* if the sign table is empty */
+    if (strcmp(head->sign, "") == 0) {
+
+        strcpy(head->sign, signStr);
+        head->dataType = type;
+        head->value = val;
+    }
+
+    else {
+
+        /* create new sign */
+        signTabPtr signTmp;
+        signTabPtr signIndex = head->next;
+        signTmp = (signTabPtr) malloc(sizeof(signTab));
+
+        if (!signTmp)
+            printError("can't allocate memory");
+
+        strcpy(signTmp->sign, signStr);
+        signTmp->dataType = type;
+        signTmp->value = val;
+
+        /* go to end of the table */
+        while (signIndex != NULL) {
+            signIndex = signIndex->next;
+        }
+
+        /* add the sign */
+        signIndex->next = &(*signTmp);
+    }
+}
+
+int isAvailable(signTabPtr head, char* signStr) {
+
+    if (strcmp(head->sign, signStr) == 0)
+        return 0;
+
+    if (head->next == NULL)
+        return 1;
+
+    return isAvailable(head->next, signStr);
+}
